78a0fe5638fe608a0da7ea7085cc2cb59270737f
Elizaveta Biryukova <birliza@mail.ru>
2022-08-12 00:15:19 +0200

    Adds show more button

--- src/main.js
+++ src/main.js
@@ -1,17 +1,42 @@
-import {createRatingTemplate} from './view/rating.js';
-import {createNavigationTemplate} from './view/navigation.js';
-import {createSortTemplate} from './view/sort.js';
-import {createFilmsTemplate} from './view/films.js';
-import {createFilmsListTemplate} from './view/films-list.js';
-import {createFilmCardTemplate} from './view/film-card.js';
-import {createShowMoreTemplate} from './view/show-more.js';
-import {createFilmsListExtraTemplate} from './view/films-list-extra.js';
-import {createStatisticsTemplate} from './view/statistics.js';
-import {createFilmDetailsTemplate} from './view/film-details.js';
-import {generateFilms} from './mock/films-mock.js';
-import { generateFilter } from './utils/filter.js';
+import {
+  createRatingTemplate
+} from './view/rating.js';
+import {
+  createNavigationTemplate
+} from './view/navigation.js';
+import {
+  createSortTemplate
+} from './view/sort.js';
+import {
+  createFilmsTemplate
+} from './view/films.js';
+import {
+  createFilmsListTemplate
+} from './view/films-list.js';
+import {
+  createFilmCardTemplate
+} from './view/film-card.js';
+import {
+  createShowMoreTemplate
+} from './view/show-more.js';
+import {
+  createFilmsListExtraTemplate
+} from './view/films-list-extra.js';
+import {
+  createStatisticsTemplate
+} from './view/statistics.js';
+import {
+  createFilmDetailsTemplate
+} from './view/film-details.js';
+import {
+  generateFilms
+} from './mock/films-mock.js';
+import {
+  generateFilter
+} from './utils/filter.js';
 
-const FILMS_COUNT = 5;
+const FILMS_COUNT = 30;
+const FILMS_COUNT_PER_STEP = 5;
 const EXTRA_FILMS = 2;
 
 const films = new Array(FILMS_COUNT).fill().map(generateFilms);
@@ -43,11 +68,30 @@ render(flimsElement, createFilmsListTemplate(), 'beforeend');
 const flimsListElement = siteMainElement.querySelector('.films-list');
 
 const flimsListContainerElement = siteMainElement.querySelector('.films-list__container');
-for (let i = 0; i < FILMS_COUNT; i++) {
+
+for (let i = 0; i < Math.min(films.length, FILMS_COUNT_PER_STEP); i++) {
   render(flimsListContainerElement, createFilmCardTemplate(films[i]), 'beforeend');
 }
 
-render(flimsListElement, createShowMoreTemplate(), 'beforeend');
+if (films.length > FILMS_COUNT_PER_STEP) {
+  let renderFilmsCount = FILMS_COUNT_PER_STEP;
+  render(flimsListElement, createShowMoreTemplate(), 'beforeend');
+
+  const showMoreButton = flimsListElement.querySelector('.films-list__show-more');
+
+  showMoreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    films
+      .slice(renderFilmsCount, renderFilmsCount + FILMS_COUNT_PER_STEP)
+      .forEach((film) => render(flimsListContainerElement, createFilmCardTemplate(film), 'beforeend'));
+
+    renderFilmsCount += FILMS_COUNT_PER_STEP;
+
+    if (renderFilmsCount >= films.length) {
+      showMoreButton.remove();
+    }
+  });
+}
 
 render(flimsElement, createFilmsListExtraTemplate('Top rated'), 'beforeend');
 render(flimsElement, createFilmsListExtraTemplate('Most commented'), 'beforeend');
